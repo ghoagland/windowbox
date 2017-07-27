8e3412c61474e81418c2fa3966770a6446fe4741
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-07-27 12:50:37 -0400

    changed component file names for clarity and updated component index to reflect

--- client/components/index.js
+++ client/components/index.js
@@ -4,7 +4,7 @@
  * having to figure out which file they belong to!
  */
 export {default as Main} from './main'
-export {default as UserHome} from './user-home'
+export {default as UserHome} from './UserHome'
 export {default as AllProducts} from './AllProducts'
-export {Login, Signup} from './auth-form'
+export {Login, Signup} from './AuthForm'
 
