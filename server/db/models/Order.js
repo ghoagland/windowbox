255b6710347d76a96f847f1c195ccf8e03230d3c
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-08-01 16:51:22 -0400

    finished order and orderproduct models and associated them and also added examples to seed

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -5,6 +5,22 @@ const Order = db.define('order', {
     status: {
         type: Sequelize.ENUM('Created', 'Processing', 'Cancelled', 'Completed'),
         allowNull: false
+    },
+    subtotal: {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false
+    },
+    sessionId: {
+      type: Sequelize.STRING,
     }
+}, {
+  validate: {
+    oneOfSessionOrUser: function() {
+      if (!this.userId && !this.sessionId) {
+        throw new Error('Must have either a user or a sessionId')
+      }
+    }
+  }
 })
+
 module.exports = Order
